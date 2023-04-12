a59c42cb7072cf8c14551185cc0391eb42031ede
choi138 <kidjustinchoi@gmail.com>
2023-04-12 18:47:02 +0900

    fix(components): footer section apply button text change

--- src/components/main/FooterSection/index.tsx
+++ src/components/main/FooterSection/index.tsx
@@ -9,7 +9,7 @@ export const FooterSection: React.FC = () => {
     <S.FooterSectionContainer>
       <Text.Column>
         <Text size="medium">멋져 보인다고요?</Text>
-        <Text size="small">HANBOOK 서비스는 부장이 신청하면 자유롭게 이용할 수 있어요!</Text>
+        <Text size="small">HANBOOK 서비스는 동아리 부장이라면 자유롭게 신청할수 있어요!</Text>
       </Text.Column>
       <S.ApplyButton to="/">서비스 신청하기</S.ApplyButton>
     </S.FooterSectionContainer>
