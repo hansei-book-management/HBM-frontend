3470f0fca2d5d19b0c5f9673b2d4574d768249fe
choi138 <kidjustinchoi@gmail.com>
2023-04-22 21:09:13 +0900

    feat(page): add user page styles

--- src/pages/manages/user/index.tsx
+++ src/pages/manages/user/index.tsx
@@ -4,8 +4,39 @@ import * as S from './styled';
 
 export const ManageUserPage: React.FC = () => {
   return (
-    <div>
-      <h1>This is Manage user page</h1>
-    </div>
+    <S.ManageUserContainer>
+      <S.ManageUserMenuBar>
+        <S.ManageUserMenuBarItem>부원</S.ManageUserMenuBarItem>
+        <S.ManageUserMenuBarItem>대여 책</S.ManageUserMenuBarItem>
+        <S.ManageUserMenuBarItem>상태</S.ManageUserMenuBarItem>
+      </S.ManageUserMenuBar>
+      <S.ManageUserInfoContainer>
+        <S.ManageUserIconContainer>
+          <S.ManageUserIcon />
+          <S.ManageUserName>박찬영</S.ManageUserName>
+        </S.ManageUserIconContainer>
+        <S.ManageUserBookInfo>0권</S.ManageUserBookInfo>
+        <S.ManageUserStatus isOk={false}>
+          대출정지 <br />
+          (8일 12시간 20분 남음)
+        </S.ManageUserStatus>
+      </S.ManageUserInfoContainer>
+      <S.ManageUserInfoContainer>
+        <S.ManageUserIconContainer>
+          <S.ManageUserIcon />
+          <S.ManageUserName>박찬영</S.ManageUserName>
+        </S.ManageUserIconContainer>
+        <S.ManageUserBookInfo>3권</S.ManageUserBookInfo>
+        <S.ManageUserStatus isOk={true}>정상</S.ManageUserStatus>
+      </S.ManageUserInfoContainer>
+      <S.ManageUserInfoContainer>
+        <S.ManageUserIconContainer>
+          <S.ManageUserIcon />
+          <S.ManageUserName>박찬영</S.ManageUserName>
+        </S.ManageUserIconContainer>
+        <S.ManageUserBookInfo>0권</S.ManageUserBookInfo>
+        <S.ManageUserStatus isOk={true}>정상</S.ManageUserStatus>
+      </S.ManageUserInfoContainer>
+    </S.ManageUserContainer>
   );
 };
--- src/pages/manages/user/styled.ts
+++ src/pages/manages/user/styled.ts
@@ -1,1 +1,60 @@
+import { FaUserCircle } from 'react-icons/fa';
+
 import styled from 'styled-components';
+
+export const ManageUserContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+`;
+
+export const ManageUserMenuBar = styled.div`
+  display: grid;
+  grid-template-columns: 2.96fr 1fr 2fr;
+  padding: 0.6rem 1rem;
+`;
+
+export const ManageUserMenuBarItem = styled.span`
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+export const ManageUserInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr 2fr;
+  padding: 1rem 1.4rem;
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 1rem;
+`;
+
+export const ManageUserIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ManageUserIcon = styled(FaUserCircle)`
+  width: 1.8rem;
+  height: 1.8rem;
+  color: ${({ theme }) => theme.primary.gray};
+`;
+
+export const ManageUserName = styled.span`
+  font-size: 1rem;
+  font-weight: 600;
+  margin-left: 1rem;
+`;
+
+export const ManageUserBookInfo = styled.span`
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+export const ManageUserStatus = styled.span<{ isOk: boolean }>`
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 1.45em;
+  color: ${({ isOk, theme }) => (isOk ? theme.primary.green : theme.primary.red)};
+`;
