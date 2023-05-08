33af2cd38381af0547a19b68f2eb5087362e53d4
choi138 <kidjustinchoi@gmail.com>
2023-05-08 20:55:18 +0900

    feat(pages): add register page input pattern

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import * as S from './styled';
@@ -6,6 +6,7 @@ import * as S from './styled';
 export interface RegisterFormValues {
   username: string;
   password: string;
+  passwordCheck: string;
   name: string;
   studentId: string;
   phone: string;
@@ -18,34 +19,43 @@ export const RegisterPage: React.FC = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm<RegisterFormValues>();
+
+  const password = useRef({});
+  password.current = watch('password', '');
+
   const [page, setPage] = useState(1);
 
-  const nextClick = () => {
-    setPage(page + 1);
-  };
-
-  const prevClick = () => {
-    setPage(page - 1);
+  const onPrevClick = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
   };
 
   const onSubmit = (data: RegisterFormValues) => {
-    console.log(data);
+    if (page === 1) {
+      setPage(page + 1);
+    } else {
+      console.log(data);
+    }
   };
   return (
     <S.RegisterWrapper>
+      {page === 2 ? (
+        <S.RegisterBackButton onClick={onPrevClick}>&larr;</S.RegisterBackButton>
+      ) : (
+        <S.RegisterBackButton style={{ opacity: '0' }}>&larr;</S.RegisterBackButton>
+      )}
       <S.RegisterContainer onSubmit={handleSubmit(onSubmit)}>
         <div>
           <S.RegisterInput
+            type="text"
             {...register('username', {
               required: '아이디는 필수입니다.',
-              maxLength: {
-                value: 20,
-                message: '아이디는 20자 이내로 입력해주세요.',
-              },
-              minLength: {
-                value: 5,
-                message: '아이디는 5자 이상 입력해주세요.',
+              pattern: {
+                value: /^[a-zA-Z0-9가-힣]{5,20}$/,
+                message: '5~20자 한글 또는 영문, 숫자를 입력해주세요',
               },
             })}
             placeholder="아이디를 입력해주세요..."
@@ -54,11 +64,12 @@ export const RegisterPage: React.FC = () => {
         </div>
         <div>
           <S.RegisterInput
+            type="password"
             {...register('password', {
               required: '비밀번호는 필수입니다.',
-              minLength: {
-                value: 5,
-                message: '비밀번호는 5자 이상 입력해주세요.',
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/,
+                message: '8~16자 영문, 숫자, 특수문자 조합을 입력해주세요',
               },
             })}
             placeholder="비밀번호를 입력해주세요..."
@@ -67,15 +78,27 @@ export const RegisterPage: React.FC = () => {
         </div>
         <div>
           <S.RegisterInput
+            type="passwordCheck"
+            {...register('passwordCheck', {
+              required: '비밀번호 확인을 입력해주세요.',
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/,
+                message: '8~16자 영문, 숫자, 특수문자 조합을 입력해주세요',
+              },
+              validate: (value) =>
+                value === password.current || '입력한 비밀번호와 일치하지 않습니다.',
+            })}
+            placeholder="비밀번호를 다시 한번 확인해주세요..."
+          />
+          <S.RegisterErrorMessage>{errors.passwordCheck?.message}</S.RegisterErrorMessage>
+        </div>
+        <div>
+          <S.RegisterInput
             {...register('name', {
               required: '이름은 필수입니다.',
-              maxLength: {
-                value: 20,
-                message: '이름은 20자 이내로 입력해주세요.',
-              },
-              minLength: {
-                value: 2,
-                message: '이름은 2자 이상 입력해주세요.',
+              pattern: {
+                value: /^[가-힣]{2,10}$/,
+                message: '2~10자 한글을 입력해주세요',
               },
             })}
             placeholder="이름을 입력해주세요..."
@@ -94,12 +117,16 @@ export const RegisterPage: React.FC = () => {
                 value: 5,
                 message: '학번은 5자 입니다.',
               },
+              pattern: {
+                value: /^[NCGH][1-3][1-2][0-30]$/, // 이 코드를 해석하면 NCGH로 시작하고 1~3학년 1~2반 0~30번까지의 학번을 입력하라는 뜻입니다.
+                message: '학번 형식이 올바르지 않습니다.',
+              },
             })}
             placeholder="학번을 입력해주세요..."
           />
           <S.RegisterErrorMessage>{errors.studentId?.message}</S.RegisterErrorMessage>
         </div>
-        <S.RegisterButton>회원가입</S.RegisterButton>
+        <S.RegisterButton>{page === 1 ? '다음' : '회원가입'}</S.RegisterButton>
       </S.RegisterContainer>
     </S.RegisterWrapper>
   );
