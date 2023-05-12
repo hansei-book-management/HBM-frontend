60f01d096ab7472a0d023354b8502923b28bddb5
최근원 <kidjustinchoi@gmail.com>
2023-05-12 10:20:57 +0900

    chore: npm run lint & format

--- src/pages/manages/club-book/index.tsx
+++ src/pages/manages/club-book/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa';
 import Lottie from 'react-lottie';
 
