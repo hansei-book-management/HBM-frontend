730ba732c7e5117dbf159bce84f975b3865a432c
최근원 <kidjustinchoi@gmail.com>
2023-06-26 14:50:02 +0900

    feat(component): add skeleton ui in book page

--- src/components/common/Skeleton/index.tsx
+++ src/components/common/Skeleton/index.tsx
@@ -3,22 +3,30 @@ import React from 'react';
 import * as S from './styled';
 
 interface SkeletonProps {
-  isRentPage: boolean;
+  isManyBook?: boolean;
 }
 
-export const Skeleton: React.FC<SkeletonProps> = ({ isRentPage }) => {
+export const Skeleton: React.FC<SkeletonProps> = ({ isManyBook = true }) => {
   return (
     <S.SkeletonContainer>
-      {[...Array(isRentPage ? 20 : 8)].map((_, index) => (
-        <S.SkeletonImageContainer key={index}>
-          <S.SkeletonImage />
-          <S.SkeletonImageInfoContainer>
-            <S.SkeletonImageTitle />
-            <S.SkeletonImageSubTitle />
-            {isRentPage && <S.SkeletonImageMessage />}
-          </S.SkeletonImageInfoContainer>
-        </S.SkeletonImageContainer>
-      ))}
+      <S.SkeletonHeaderSection>
+        <S.SkeletonHeaderSectionList>
+          {[...Array(4)].map((_, index) => (
+            <S.SkeletonHeaderSectionItem key={index} />
+          ))}
+        </S.SkeletonHeaderSectionList>
+      </S.SkeletonHeaderSection>
+      <S.SkeletonMainSection>
+        {[...Array(isManyBook ? 20 : 8)].map((_, index) => (
+          <S.SkeletonImageContainer key={index}>
+            <S.SkeletonImage />
+            <S.SkeletonImageInfoContainer>
+              <S.SkeletonImageTitle />
+              <S.SkeletonImageSubTitle />
+            </S.SkeletonImageInfoContainer>
+          </S.SkeletonImageContainer>
+        ))}
+      </S.SkeletonMainSection>
     </S.SkeletonContainer>
   );
 };
